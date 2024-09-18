b500926eead39010548bac4ba95935f320fe0717
AmanMalviya22 <amanaman26802@gmail.com>
2024-09-18 10:39:18 +0530

    updated

--- deploy.sh
+++ deploy.sh
@@ -21,7 +21,7 @@ git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-git push -f https://github.com/kaz-yamada/vue-sudoku.git  master:gh-pages
+git push -f https://github.com/AmanMalviya22/sudoku.git  master:gh-pages
 
 
 cd -
